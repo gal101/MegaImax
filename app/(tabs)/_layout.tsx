b9c204d0ba1b8283adc9aae55ff6e6d26ebd2f6a
Andrei Galateanu <andreidan.galateanu@gmail.com>
2024-12-08 06:52:23 +0200

    Added red color to tab icons and button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: 'rgb(235,44,28)',
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
